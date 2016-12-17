4dc8004c2d9155f450aa9c03befbc3ce009c18db
Anthony Delgado <me@anthonydelgado.me>
2016-12-17 12:01:44 -0500

    Example React Hello World App

---
+++ app/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by user on 12/17/16.
+ */
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render( <p>Hello World</p>, document.getElementById("app"));
